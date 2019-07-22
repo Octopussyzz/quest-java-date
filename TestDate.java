0bf5f1cdc975f976888dbee86496dd05732fa214
Octopussyzz <spudd.otw@gmail.com>
2019-07-22 11:35:58 +0200

    DDAY

--- TestDate.java
+++ TestDate.java
@@ -17,7 +17,11 @@ class TestDate {
         int year = sc.nextInt();
         sc.close();
 
-        // TODO : your code after this line
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = new GregorianCalendar(year, month - 1, day);
+        Date d = c.getTime();
+        int dayW = c.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The date is : " + sdf.format(d) + ". The day of the week is : " + (dayW - 1));
         
     }
 }
